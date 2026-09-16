5dcc10a823d9d13dc22cf1c429e2fed24638ef6c
Lucas-hrl <lucas.herchuel@uha.fr>
2026-09-16 09:26:10 +0200

    Add files via upload

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -34,3 +34,18 @@ export const uploadVideo = multer({
   fileFilter,
   limits: { fileSize: 50 * 1024 * 1024 } // Limite à 50MB par exemple
 });
+
+// À ajouter dans upload.middleware.js
+const imageFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb(new Error('Le fichier doit être une image.'));
+  }
+};
+
+export const uploadPhoto = multer({ 
+  storage: storage, // On réutilise son 'storage' !
+  fileFilter: imageFilter,
+  limits: { fileSize: 10 * 1024 * 1024 } // 10 Mo max pour une photo
+});
